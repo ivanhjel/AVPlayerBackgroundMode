d8844c2958a306ce9d6ee3b010167425f553ae3f
Ivan Lé Hjelmeland <ivan.hjelmeland@shortcut.no>
2019-05-14 23:44:41 +0200

    init Xcode new project template.

---
+++ AVPlayerBackgroundMode/AVPlayerBackgroundMode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVPlayerBackgroundMode
+//
+//  Created by Ivan Lé Hjelmeland on 14/05/2019.
+//  Copyright © 2019 AboveApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
